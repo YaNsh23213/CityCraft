96b902d2618d827f5fbdc5ed81aa559c423af70c
YaNsh23213 <72270273+YaNsh23213@users.noreply.github.com>
2023-11-22 01:24:34 +0200

    AddMesh

---
+++ Source/CityCraft/Private/Dev/PTBaseMinigameWidget.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Dev/PTBaseMinigameWidget.h"
+
+void UPTBaseMinigameWidget::NativeConstruct() {}
---
+++ Source/CityCraft/Public/Dev/PTBaseMinigameWidget.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PTBaseMinigameWidget.generated.h"
+
+UCLASS()
+class CITYCRAFT_API UPTBaseMinigameWidget : public UUserWidget
+{
+    GENERATED_BODY()
+
+protected:
+
+    virtual void NativeConstruct() override;
+
+
+};
